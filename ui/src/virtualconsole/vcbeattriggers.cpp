77c76b8c3d06ec4771999f9dbbcd7de9e3cfe203
horstsoft0815 <sebastian.moeckel@web.de>
2021-12-12 14:35:04 +0100

    #8 added missing parts of sampelrate and btrack projects, renamed libsamplerate to samplerate

---
+++ ui/src/virtualconsole/vcbeattriggers.cpp
@@ -0,0 +1,6 @@
+#include "vcbeattriggers.h"
+
+vcbeattriggers::vcbeattriggers()
+{
+
+}
---
+++ ui/src/virtualconsole/vcbeattriggers.h
@@ -0,0 +1,11 @@
+#ifndef VCBEATTRIGGERS_H
+#define VCBEATTRIGGERS_H
+
+
+class vcbeattriggers
+{
+public:
+    vcbeattriggers();
+};
+
+#endif // VCBEATTRIGGERS_H
